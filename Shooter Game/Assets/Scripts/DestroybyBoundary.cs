a7ffc9a0a9e5772e094c8fe688260f9e0b26d782
Emily Moritz <fullmetal1104@gmail.com>
2019-07-29 21:30:05 -0400

    please work
    i've tried so many times please

---
+++ Shooter Game/Assets/Scripts/DestroybyBoundary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroybyBoundary : MonoBehaviour
+{
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
