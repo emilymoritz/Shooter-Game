2635e083cb3edb2b25403604ba1f9ed0d782d83b
Emily Moritz <fullmetal1104@gmail.com>
2019-08-01 13:54:20 -0400

    please

--- Shooter Game/Assets/Scripts/GameController.cs
+++ Shooter Game/Assets/Scripts/GameController.cs
@@ -7,20 +7,21 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
 
-    public Text ScoreText;
+    public Text PointText;
     public Text restartText;
     public Text gameOverText;
+    public Text creditsText;
 
     private bool gameOver;
     private bool restart;
-    private int score;
+    private int points;
 
     void Start()
     {
@@ -28,8 +29,9 @@ public class GameController : MonoBehaviour
         restart = false;
         restartText.text = "";
         gameOverText.text = "";
-        score = 0;
-        UpdateScore();
+        creditsText.text = "";
+        points = 0;
+        Update();
         StartCoroutine (SpawnWaves());
 
         if (Input.GetKey("escape"))
@@ -47,6 +49,7 @@ public class GameController : MonoBehaviour
         }
     }
 
+
     IEnumerator SpawnWaves ()
     {
         yield return new WaitForSeconds(startWait);
@@ -54,6 +57,7 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range (0,hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
@@ -70,16 +74,24 @@ public class GameController : MonoBehaviour
         }
     }
 
-    public void AddScore(int newScoreValue)
+    public void AddPoints(int newPointsValue)
     {
-        score += newScoreValue;
-        UpdateScore();
+        points += newPointsValue;
+        UpdatePoints();
     }
 
-    void UpdateScore()
+    void UpdatePoints()
     {
-        ScoreText.text = "Score: " + score;
-    }
+        PointText.text = "Points: " + points;
+
+            if (points >= 100)
+            {
+                gameOverText.text = "You win!";
+                gameOver = true;
+                restart = true;
+                creditsText.text = "Game Created By Emily Moritz";
+            }
+     }
 
     public void GameOver ()
     {
